45f93220f80e567e52db4d78a0faba4c8b4f9907
kevin <kevingao52997@gmail.com>
2024-10-01 15:40:48 -0700

    removed default landing page, added redux and a store file

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,24 +1,38 @@
 import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { BrowserRouter, 
+        Route, 
+        Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
+  const dispatch = useDispatch();
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className='main container'>
+        <div className='navbar container'>
+          Navbar Placeholder
+        </div>
+        <div className='body container'>
+          <Routes>
+            <Route path='/'>
+              Homepage
+            </Route>
+            <Route path='/about'>
+              About Us
+            </Route>
+            <Route path='/shop'>
+              Shop
+            </Route>
+            <Route path='/contact'>
+              Contact Us
+            </Route>
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 }
 
