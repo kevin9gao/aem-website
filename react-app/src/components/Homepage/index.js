e634084c822706e68b3da85784e4f7ac91809020
kevin <kevingao52997@gmail.com>
2024-10-08 14:36:50 -0700

    home page done for now

--- react-app/src/components/Homepage/index.js
+++ react-app/src/components/Homepage/index.js
@@ -1,9 +1,13 @@
 import React, { useEffect } from "react";
 // import { useDispatch } from "react-redux";
 import "./Home.css";
+import { useNavigate } from "react-router-dom";
 
 function HomePage() {
     // const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+
     useEffect(() => {
         const title = document.getElementsByClassName('split');
         for (let i = 0; i < title.length; i++) {
@@ -32,6 +36,17 @@ function HomePage() {
                             </div>
                         </div>
                     </h1>
+                    <div className="tagline-container">
+                        <p className="tagline animate-2">
+                            Nature’s best for your gut health and vitality.
+                        </p>
+                        <button className="call-to-action animate-2" onClick={() => navigate('/about-us')}>
+                            Learn More
+                        </button>
+                        <button className="call-to-action animate-2" onClick={() => navigate('/shop')}>
+                            Shop Now
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
