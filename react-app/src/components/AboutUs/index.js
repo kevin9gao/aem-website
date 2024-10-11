ae1fae83e43e1f39ecac5f45636856929304d9c0
kevin <kevingao52997@gmail.com>
2024-10-11 16:32:52 -0700

    started on about us and changed AEMBioUSA to AEMBIOUSA in navbar

---
+++ react-app/src/components/AboutUs/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import './About.css';
+
+function AboutUs() {
+
+
+  return (
+    <div className="about container">
+      <div className="about">
+        <h1>About Us</h1>
+      </div>
+    </div>
+  );
+}
+
+export default AboutUs;
